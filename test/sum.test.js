b9baebf35caa1af66440642e76ad6b494b4fc0f3
ArthurRaynal <arthur.raynal@le-campus-numerique.fr>
2022-01-27 09:44:05 +0100

    integration continue

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,14 @@
 // Add the unit tests of the sum function here
 import {sum} from "../src/sum";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe("The sum function", function(){
+
+  it("should receive two numbers as arguments", function(){
+    expect(sum("a", "test")).toBe("You must provide 2 numbers!")
+  })
+
+  test('adds 1 + 2 to equal 3', function() {
+    expect(sum(1, 2)).toBe(3)
+  })
+
+})
